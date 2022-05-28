034d724e7c3c3994bf9bb886085f1b18312fc151
ranirizkiani <ranirizkiani1@gmail.com>
2022-05-28 21:47:57 +0700

    modified index.js

--- index.js
+++ index.js
@@ -23,19 +23,39 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  for (const pegawai of dataYangAkanDilooping) {
+    // const pegawai = {
+    //   id: 97,
+    //   namaDepan: 'Nolan',
+    //   namaBelakang: 'Zieme',
+    //   jenisKelamin: 'F'
+    // }
+    const namaPanjang = pegawai.namaDepan + " " + pegawai.namaBelakang; // "Nolan Zieme"
+    hasilLooping.push(namaPanjang); // ["Nolan Zieme"]
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+  for (const pegawai of dataYangAkanDilooping) {
+    if (pegawai.jenisKelamin == 'M') {
+      jumlahPria++;
+    }
+  }
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
+  for (const pegawai of dataYangAkanDilooping) {
+    if (pegawai.jenisKelamin == 'F') {
+      jumlahWanita++;
+    }
+  }
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +68,14 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar =  "Jumlah Wanita lebih banyak dari Pria";
+  } else if (jumlahPria == jumlahWanita) {
+    komentar =  "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
